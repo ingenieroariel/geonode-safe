714ca19566e0858ff5bd234c33e66ba5498819b7
Ariel Núñez <ingenieroariel@gmail.com>
2012-08-14 15:12:21 +0000

    Re-enabled urls for geonode_safe

--- geonode_safe/urls.py
+++ geonode_safe/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
                            {'template': 'geonode_safe/calculator.html'},
                            name='calculator'))
 
-urlpatterns += patterns('',
-                       url(r'^api/calculate/$', 'geonode_safe.views.debug', name='safe-calculate'),
+urlpatterns += patterns('geonode_safe.views',
+                       url(r'^api/calculate/$', 'calculate', name='safe-calculate'),
                        url(r'^api/layers/$', 'layers', name='safe-layers'),
                        url(r'^api/functions/$', 'functions', name='safe-functions'),
                        url(r'^api/debug/$', 'debug', name='safe-debug'),
