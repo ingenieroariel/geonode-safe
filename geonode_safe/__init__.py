ca5377563f884a5eb8aaa090c7e5c1419b70fc10
Ariel Núñez <ingenieroariel@gmail.com>
2012-08-21 22:01:46 +0000

    Added version information

--- geonode_safe/__init__.py
+++ geonode_safe/__init__.py
@@ -1,5 +1,5 @@
 __version__= (0, 1, 0, 'alpha', 0)
 
 def get_version():
-    from safe.common.version import get_version
+    from geonode_safe.version import get_version
     return get_version(__version__)
